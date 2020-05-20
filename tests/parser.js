8c54a0b505224e7a79e3c6fa1e72263e16b99ad9
Brett Zamir <brettz9@yahoo.com>
2020-05-20 15:20:33 +0800

    Just testing for issue 67

--- tests/parser.js
+++ tests/parser.js
@@ -1,6 +1,19 @@
 import esquery from "../esquery.js";
+import * as espree from 'espree';
 
 describe("basic query parsing", function () {
+
+    it.only('test experimental spread', function () {
+        const parsed = espree.parse(`
+        const a = {};
+        const b = { ...a };
+        `, {
+            ecmaVersion: 2018
+        });
+        console.log('parsed', parsed.body[1].declarations[0].init);
+        esquery.query(parsed, 'CallExpression[callee.name=/^(invokeMap|get|has|hasIn|invoke|result|set|setWith|unset|update|updateWith)$/] > :not(ArrayExpression):nth-child(2)');
+    });
+
 
     it("empty query", function () {
         assert.equal(void 0, esquery.parse(""));
